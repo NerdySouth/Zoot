b0d4c4118fe5241949e882d12014b89cae973361
Tristen Nollman <tristen.nollman@gmail.com>
2022-08-22 01:06:23 -0700

    blink on board LED works, moving on to uart

--- src/gpio.zig
+++ src/gpio.zig
@@ -1,5 +1,6 @@
 const mmio = @import("mmio.zig");
-
+/// A Gpio struct is made up of all the various MMIO registers associated with
+/// a given GpioBase. See the RK3399 TRM for more info. Chapter 20 in my copy.
 pub const Gpio = struct {
     data: mmio.Register(u32, u32),
     dir: mmio.Register(u32, u32),
@@ -12,6 +13,8 @@ pub const Gpio = struct {
     }
 };
 
+/// A GpioBase is just an address to the base of a given set of GPIO MMIO
+/// registers as defined in the RK3399 TRM chapter 20.
 pub const GpioBase = enum(u32) {
     zero = 0xFF720000,
     one = 0xFF730000,
